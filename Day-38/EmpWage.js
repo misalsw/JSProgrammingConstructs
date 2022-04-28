a2fc948a30ff8add672ba7b54f06fba9122b994f
swap123 <misalswapnil77@gmail.com>
2022-04-28 15:00:27 +0530

    Calculating Wages till no of days or Working Hours Reached

--- Day-38/EmpWage.js
+++ Day-38/EmpWage.js
@@ -1,3 +1,4 @@
+//constant variables
 const IS_ABSENT = 0;
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
@@ -5,7 +6,14 @@ const EMP_WAGE_PHR = 20;
 const PART_TIME_HRS = 4;
 const FULL_TIME_HRS = 8;
 const NUM_OF_WORKING_DAYS = 20;
+const MAX_HRS_IN_MONTH = 160;
 
+//temp variables
+let empHrs = 0;
+let totalWorkingHrs = 0;
+let totalWorkingDays = 0;
+
+/** getting employee hours **/
 function getEmpHrs(empCheck) {
   switch (empCheck) {
     case IS_PART_TIME:
@@ -19,11 +27,22 @@ function getEmpHrs(empCheck) {
   }
 }
 
-let empHrs = 0;
-for (let i = 0; i < NUM_OF_WORKING_DAYS; i++) {
+while (
+  totalWorkingHrs <= MAX_HRS_IN_MONTH &&
+  totalWorkingDays < NUM_OF_WORKING_DAYS
+) {
+  totalWorkingDays++;
+
   let empCheck = Math.floor(Math.random() * 3);
   empHrs += getEmpHrs(empCheck);
 }
 
 let empWage = empHrs * EMP_WAGE_PHR; //Calculating wage.
-console.log("Employee Hours :- " + empHrs + " & Employee Wage :- " + empWage);
+console.log(
+  "Total Days :- " +
+    totalWorkingDays +
+    " Employee Hours :- " +
+    empHrs +
+    " & Employee Wage :- " +
+    empWage
+);
